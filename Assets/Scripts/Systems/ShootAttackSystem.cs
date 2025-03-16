64a0b03c269c816f7cac971a128649e3fe393f03
박서진 <kevin8193@naver.com>
2025-03-16 12:55:06 +0900

    init

---
+++ Assets/Scripts/Systems/ShootAttackSystem.cs
@@ -0,0 +1,43 @@
+using Unity.Burst;
+using Unity.Entities;
+using Unity.Transforms;
+using UnityEngine;
+
+partial struct ShootAttackSystem : ISystem {
+
+    [BurstCompile]
+    public void OnUpdate(ref SystemState state) {
+        EntitiesReferences entitiesReferences = SystemAPI.GetSingleton<EntitiesReferences>();
+
+        foreach ((
+            RefRO<LocalTransform> localTransform,
+            RefRW <ShootAttack> shootAttack,
+            RefRO<Target> target)
+            in SystemAPI.Query<
+                RefRO<LocalTransform>,
+                RefRW<ShootAttack>,
+                RefRO<Target>>()) {
+
+            if (target.ValueRO.targetEntity == Entity.Null) {
+                continue;
+            }
+
+            shootAttack.ValueRW.timer -= SystemAPI.Time.DeltaTime;
+            if (shootAttack.ValueRO.timer > 0f) {
+                continue;
+            }
+            shootAttack.ValueRW.timer = shootAttack.ValueRO.timerMax;
+
+            Entity bulletEntity = state.EntityManager.Instantiate(entitiesReferences.bulletPrefabEntity);
+            SystemAPI.SetComponent(bulletEntity, LocalTransform.FromPosition(localTransform.ValueRO.Position));
+
+            RefRW<Bullet> bulletBullet = SystemAPI.GetComponentRW<Bullet>(bulletEntity);
+            bulletBullet.ValueRW.damageAmount = shootAttack.ValueRO.damageAmount;
+
+            RefRW<Target> bulletTarget = SystemAPI.GetComponentRW<Target>(bulletEntity);
+            bulletTarget.ValueRW.targetEntity = target.ValueRO.targetEntity;
+
+        }
+    }
+
+}
